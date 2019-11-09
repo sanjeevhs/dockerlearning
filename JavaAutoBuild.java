c03d7805f1b54dafd41fb300892c324b1849c397
sanjeevhs <sanjivhs16@gmail.com>
2019-11-09 19:35:21 +0530

    Learning1

--- JavaAutoBuild.java
+++ JavaAutoBuild.java
@@ -3,6 +3,6 @@ public class JavaAutoBuild{
 public static void main(String[] args){
 
 System.out.println("Hello Docker Workshop");
-System.out.println("This is automated world");
+System.out.println("This is automated world, keep learning");
 }
 }
